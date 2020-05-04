7f28a6ce6235b1352418effdcb0034a08a9dd5e2
AzaniaBG <azaniazbaker@gmail.com>
2020-05-04 08:17:57 -0700

    modified .eslintrc config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,28 @@
 module.exports = {
-    module: {
-        settings: {
-            "extends": "./node_modules/coding-standard/.pug-lintrc",
-            "disallowIdLiterals": null
-        },
-        rules: [
-            {
-              enforce: 'pre',
-              test: /\.(js|vue)$/,
-              loader: 'eslint-loader',
-              exclude: /node_modules/
-            }
-          ]          
+    root: true,
+    settings: {
+
     },
-        plugins: [
-            "pug"
-        ]
-    
-}
+    env: {
+        // this section will be used to determine which APIs are available to us
+        // (i.e are we running in a browser environment or a node.js env)
+        node: true,
+        browser: true,
+        es6: true
+    },
+    parserOptions: {
+        parser: "babel-eslint",
+        // specifying a module sourcetype prevent eslint from marking import statements as errors
+        sourceType: "module"
+    },
+    extends: [
+        // use the recommended rule set for both plain javascript and vue
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    rules: {
+        // we should always disable console logs and debugging in production
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    }
+};
