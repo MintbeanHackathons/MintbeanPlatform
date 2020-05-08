c123f5771df0919cf25cffb5dd2e6d9d77d2f4ef
Andrew Horn <arhorn@smcm.edu>
2020-05-08 11:54:38 -0500

    put in required colon

--- backend/shared/contributors.js
+++ backend/shared/contributors.js
@@ -2,7 +2,7 @@ const linkedIn = {
     'sophyphreak': 'https://www.linkedin.com/in/sophyphreak/',
     'monarchwadia': 'https://www.linkedin.com/in/monarchwadia/',
     'leanlunio': 'https://www.linkedin.com/in/leanjunio/',
-    'shohei-shibata:' 'https://www.linkedin.com/in/shohei-shibata-53867a41/',
+    'shohei-shibata': 'https://www.linkedin.com/in/shohei-shibata-53867a41/',
     'carloshdelreal': 'https://www.linkedin.com/in/carloshdelreal/',
 
 }
