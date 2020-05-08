52ebae1cfe5c6dcea92b9a148a65974a9cef1f53
Andrew Horn <arhorn@smcm.edu>
2020-05-08 11:33:30 -0500

    Added rest of linkedIn links for contributors
    Co-authored-by: Carlos Del Real <carloshdelreal@gmail.com>

--- backend/shared/contributors.js
+++ backend/shared/contributors.js
@@ -1,5 +1,10 @@
 const linkedIn = {
-    'sophyphreak': 'https://www.linkedin.com/in/sophyphreak/'
+    'sophyphreak': 'https://www.linkedin.com/in/sophyphreak/',
+    'monarchwadia': 'https://www.linkedin.com/in/monarchwadia/',
+    'leanlunio': 'https://www.linkedin.com/in/leanjunio/',
+    'shohei-shibata:' 'https://www.linkedin.com/in/shohei-shibata-53867a41/',
+    'carloshdelreal': 'https://www.linkedin.com/in/carloshdelreal/',
+
 }
 
 module.exports = linkedIn
